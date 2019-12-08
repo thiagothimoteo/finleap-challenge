19ac4f635ded8b3b50c4bdf39bc5441a231f3f63
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2019-12-08 19:31:04 -0300

    Improve cities reducers

--- src/reducers/cities/index.js
+++ src/reducers/cities/index.js
@@ -1,41 +1,28 @@
-export const initialState = {}
+export const initialState = []
 
 export default (state = initialState, action) => {
   switch(action.type) {
     case 'LOAD_CITIES':
-      return {
-        ...state,
-        cities: action.cities
-      }
+      return [...state, ...action.cities]
     case 'ADD_CITY':
-      return {
-        ...state,
-        cities: state.cities.map(city => {
-          if (city.id.toString() === action.cityID) {
-            city.isActive = true
-          }
+      return state.map(city => {
+        if (city.id.toString() === action.cityID) {
+          city.isActive = true
+        }
+        return city
+      })
 
-          return city
-        })
-      }
     case 'REMOVE_CITY':
-      return {
-        ...state,
-        cities: state.cities.map(city => {
-          if (city.id === action.cityID) {
-            city.isActive = false
-          }
-
-          return city
-        })
-      }
+      return state.map(city => {
+        if (city.id === action.cityID) {
+          city.isActive = false
+        }
+        return city
+      })
     case 'ORDER_CITIES_BY_MAX_TEMPERATURE':
-      return {
-        ...state,
-        cities: state.cities.concat().sort((cityA, cityB) =>
-          cityB.tempMax - cityA.tempMax
-        )
-      }
+      return state.concat().sort((cityA, cityB) =>
+        cityB.tempMax - cityA.tempMax
+      )
     default:
       return state
   }
