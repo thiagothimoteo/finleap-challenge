c9144702b4fde8d6b2626e75c02294c823d93c91
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2019-12-09 21:14:27 -0300

    Fix App spec

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, waitForElementToBeRemoved } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('renders correctly', async () => {
+    const { getByText, getByPlaceholderText, container } = render(<App />)
+
+    const title = getByText(/Weather Monster/i)
+    const input = getByPlaceholderText(/Type the name of a city/i)
+    const blankslate = getByText("There are no cities selected :(")
+
+    expect(title).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(blankslate).toBeInTheDocument()
+
+    await waitForElementToBeRemoved(() => getByText("There are no cities selected :("))
+
+    const citiesList = container.querySelector('[class^="container"] > [class^="cardList"]')
+
+    expect(title).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(citiesList).toBeInTheDocument()
+    expect(citiesList.children.length).toBe(10)
+  });
+})
